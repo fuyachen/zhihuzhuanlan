7da89182c4ad137a4da973099e5079461ffcbbec
fuyachen <294143413@qq.com>
2023-03-16 18:35:35 +0800

    ColumnList专栏列表组件

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 mb-4 text-center shadow border-light">
+        <div class="card-body">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="w-25 my-4 border border-light rounded-circle"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-start my-4">{{ column.description }}</p>
+          <a href="#" class="btn btn-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+import imgUrl from "@/assets/column.jpg"
+
+// 定义单个专栏的数据结构
+export interface ColumnProps {
+  id: number //每个专栏的id
+  avatar?: string //头像图片地址
+  title: string //专栏标题
+  description: string //简介
+}
+
+// 父组件传来的专栏列表，是一个类型为ColumnProps的数组
+const props = defineProps<{
+  list: ColumnProps[]
+}>()
+
+// 当用户没有上传头像时，显示默认的图片
+const columnList = computed(() => {
+  return props.list.map((column) => {
+    if (!column.avatar) {
+      column.avatar = imgUrl
+    }
+    return column
+  })
+})
+</script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import eslintPlugin from "vite-plugin-eslint"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
       include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
     }),
   ],
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), //路径别名
+    },
+  },
 })
