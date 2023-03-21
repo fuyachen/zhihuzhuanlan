911dacc2e307d7378b4d94313b574837d8e8fa7c
fuyachen <294143413@qq.com>
2023-03-22 00:35:38 +0800

    useRoute获取url参数

---
+++ src/components/ColumnDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    <pre>{{ route }}</pre>
+  </h1>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router"
+
+const route = useRoute()
+</script>
+
+<style scoped></style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "./views/Home.vue"
 import Login from "./views/Login.vue"
+import ColumnDetail from "./components/ColumnDetail.vue"
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     name: "login",
     path: "/login",
     component: Login,
+  },
+  {
+    name: "column",
+    path: "/column/:id",
+    component: ColumnDetail,
   },
 ]
 
